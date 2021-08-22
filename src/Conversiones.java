9b8707216a59902f53c4601b8a296dc2ff0a874f
PDItsPerlaGarcia <perladenisser@gmail.com>
2021-08-22 17:09:46 -0600

    Conversiones de string a booleano

---
+++ src/Conversiones.java
@@ -0,0 +1,7 @@
+public class Conversiones {
+    public static void main(String[] args) {
+        String boo = "True";
+        boolean prueba = Boolean.parseBoolean(boo);
+        System.out.println("prueba = " + prueba);
+    }
+}
